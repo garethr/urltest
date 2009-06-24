b1b211bedc3fdf15d5548540fef5d54f71128704
Gareth Rushgrove <gareth@morethanseven.net>
2009-06-24 21:25:49 +0100

    modifications for uploading to pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name = "urltest",
     version = "0.1",
-    
+    author = "Gareth Rushgrove",
+    author_email = "gareth@morethanseven.net",    
+    url = "http://github.com/garethr/urltest",
     packages = find_packages('src'),
     package_dir = {'':'src'},
 )
